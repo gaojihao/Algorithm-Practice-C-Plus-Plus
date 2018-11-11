2db5ff21334422edae443185131382c20e0a967b
lizhi <lizhi1026@126.com>
2018-11-11 20:03:13 +0800

    jump Algorithm

--- AlgorithmPractice/Other.cpp
+++ AlgorithmPractice/Other.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "Other.hpp"
+
+
+int jump1(int n){
+    if (n <= 2) {
+        return n;
+    }
+    
+    return jump1(n-1)+jump1(n-2);
+};
+
+int jump2(int n){
+    if (n <= 2) {
+        return n;
+    }
+    return 2*jump1(n-1);
+};
